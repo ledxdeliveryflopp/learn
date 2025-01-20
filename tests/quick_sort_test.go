b15d4aa510d71086a89ea0bbc5ea380ee0d27201
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2025-01-21 02:06:48 +0700

    Обновил workflow

--- tests/quick_sort_test.go
+++ tests/quick_sort_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestQuickSort(t *testing.T) {
 	testList := []int{1, 4, 5, 12, 3, 2, 6, 1, 4, 13}
-	expectedList := []int{1, 1, 2, 3, 4, 4, 5, 6, 12}
+	expectedList := []int{1, 1, 2, 3, 4, 4, 5, 6, 12, 13}
 	result := algorithms.QuickSort(testList)
 	for index, value := range result {
 		if value != expectedList[index] {
