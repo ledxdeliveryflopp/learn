3cebf14f7fe7245bc72e28e20fb14534fb5f5501
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2025-01-21 02:16:48 +0700

    Проверка CI

--- main.go
+++ main.go
@@ -20,16 +20,14 @@ func main() {
 	//log.Println("new list: ", arr)
 	//log.Println("new list len: ", len(arr))
 	sortedArr := algorithms.QuickSort([]int{1, 4, 5, 12, 3, 2, 6, 1, 4, 13}) // сортировка массива
-	//log.Println("test", algorithms.QuickSort([]int{1, 4, 5, 12, 3, 2, 6, 1, 4, 13}))
-	//log.Println("test2", algorithms.QuickSortTest([]int{1, 4, 5, 12, 3, 2, 6, 1, 4, 13}))
 	log.Println("========================================")
 	log.Println("sorted list: ", sortedArr)
-	//log.Println("sorted list len: ", len(sortedArr))
-	//index, value, status := algorithms.BinarySearch(sortedArr, 21) // бинарный поиск
-	//if status == true {
-	//	log.Printf("index: %d, value: %d ", index, value)
-	//	log.Printf("check, get value by index: %d", sortedArr[index])
-	//}
+	log.Println("sorted list len: ", len(sortedArr))
+	index, value, status := algorithms.BinarySearch(sortedArr, 21) // бинарный поиск
+	if status == true {
+		log.Printf("index: %d, value: %d ", index, value)
+		log.Printf("check, get value by index: %d", sortedArr[index])
+	}
 	//algorithms.BFSSearchTest()
 }
 
